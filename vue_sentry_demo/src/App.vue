e7fec4f774538d7b1bff0d7e4d2f4e9a82114161
“fuhaojun” <fuhaojun@52tt.com>
2023-02-09 14:55:52 +0800

    feat: '初始化项目并进行sentry 上报'

---
+++ vue_sentry_demo/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h1>Vue 项目 Sentry 上报错误搭建及测试</h1>
+    <button @click="throwError">Throw error</button>
+    <button @click="throwBadRequest">模拟错误请求</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  methods: {
+    throwError() {
+      console.log('1')
+      throw new Error('Sentry Error333')
+    },
+    async throwBadRequest() {
+      console.error('Sentry Error Mock')
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button {
+  width: 100px;
+  height: 50px;
+  color: red;
+  border-radius: 10%;
+  cursor: pointer;
+}
+</style>
---
+++ vue_sentry_demo/src/main.js
@@ -0,0 +1,33 @@
+import Vue from "vue";
+import App from './App.vue'
+import Router from "vue-router";
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
+
+Vue.use(Router);
+
+const router = new Router({
+  // ...
+});
+
+Sentry.init({
+  Vue,
+  dsn: "https://4f3f6cf238394d34a455a7aa40bac565@o1407965.ingest.sentry.io/4504647894761472",
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracePropagationTargets: ["localhost", "my-site-url.com", /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// ...
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount("#app");
